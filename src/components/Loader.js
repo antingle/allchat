8a7b4602d4449ff989c8266582c0728aa5434eef
Anthony Ingle <ingle.anthony@icloud.com>
2021-12-29 15:32:59 -0500

    centered loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,5 +3,9 @@ import "../css/loader.css";
 
 // simple css loader
 export default function Loader() {
-  return <div className="loader" />;
+  return (
+    <div className="flex-screen">
+      <div className="loader" />
+    </div>
+  );
 }
